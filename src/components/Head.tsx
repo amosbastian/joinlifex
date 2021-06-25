0ec7e91d45dc2c180e7a4d37079cb5f589241996
Amos Bastian <amosbastian@gmail.com>
2021-06-25 14:34:42 +0200

    Update fonts

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -13,6 +13,16 @@ export const Head = () => {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fraunces:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+          crossOrigin="anonymous"
+        />
       </NextHead>
     </>
   );
--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,5 +1,8 @@
 import { extendTheme } from "@chakra-ui/react";
+import { foundations } from "./foundations";
 
-const overrides = {};
+const overrides = {
+  ...foundations,
+};
 
 export const theme = extendTheme(overrides);
